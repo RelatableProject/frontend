beb35e47db7b98fdb90929df04c48f5ac49bceb0
Juan David Campiño <51058847+Ingeniosin@users.noreply.github.com>
2022-04-21 01:22:41 -0500

    .

---
+++ src/graphql/utils.js
@@ -0,0 +1,17 @@
+﻿type Type = 'String' | 'Int' | 'String!' | 'Int!';
+
+type Output = {
+    name: string,
+    include: [Output]
+}
+
+type Query = {
+  inputs: {
+      [key: string]: Type;
+  },
+  outputs: [Output]
+}
+
+const getQuery = ({inputs, name, outputs}) => {
+
+}
